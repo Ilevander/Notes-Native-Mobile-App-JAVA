d1d57520a6c9c35f12f22aae4e8756512729387f
dell <dell@DESKTOP-4P8U8AR>
2024-03-26 22:00:51 +0100

    Add new note Screen design

--- app/src/main/java/com/notes/code/MainActivity.java
+++ app/src/main/java/com/notes/code/MainActivity.java
@@ -1,5 +1,6 @@
 package com.notes.code;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton addNoteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+
+        addNoteBtn.setOnClickListener((v)->startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)));
+
 
     }
 }
